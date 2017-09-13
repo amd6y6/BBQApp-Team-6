0870efa600b68156f8a61e67f8d887db9e1d30f9
Austin Dotto <austindotto@mwc-106026.dhcp.missouri.edu>
2017-09-13 15:45:21 -0500

    Created project

---
+++ BBQApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BBQApp
+//
+//  Created by Jared Bruemmer on 9/13/17.
+//  Copyright © 2017 Jared Bruemmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
