3dc1b101950f803aa5e24adf13993aa1c47763ca
christopheraking <cakd47@mail.missouri.edu>
2017-11-08 14:00:24 -0600

    Add files via upload
    added view controllers

---
+++ BBQApp/YelpAPI/YLPSearchTableViewController.m
@@ -0,0 +1,66 @@
+//
+//  YLPSearchTableViewController.m
+//  YelpAPI
+//
+//  Created by David Chen on 3/31/16.
+//  Copyright © 2016 Yelp. All rights reserved.
+//
+
+#import "YLPSearchTableViewController.h"
+#import "YLPDetailBusinessViewController.h"
+//#import "YLPAppDelegate.h"
+
+#import "YLPClient+Search.h"
+#import "YLPSortType.h"
+#import "YLPSearch.h"
+#import "YLPBusiness.h"
+
+#import "BBQApp-Swift.h"
+
+@interface YLPSearchTableViewController ()
+@property (nonatomic) YLPSearch *search;
+@end
+
+@implementation YLPSearchTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [[AppDelegate sharedClient] searchWithLocation:@"Columbia, MO" term:nil limit:50 offset:0 categoryFilter:@[@"bbq"] sort:YLPSortTypeDistance completionHandler:^
+        (YLPSearch *search, NSError* error) {
+            self.search = search;
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.tableView reloadData];
+            });
+    }];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.search.businesses.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SearchCell" forIndexPath:indexPath];
+    if (indexPath.item > [self.search.businesses count]) {
+        cell.textLabel.text = @"";
+    }
+    else {
+        cell.textLabel.text = self.search.businesses[indexPath.item].name;
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    YLPDetailBusinessViewController *vc = [self.storyboard instantiateViewControllerWithIdentifier:@"YLPDetailBusinessViewController"];
+    vc.business = self.search.businesses[indexPath.item];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+@end
